f4f8564addfde08ce49c7869af1d05a533c430f6
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-04-08 14:54:20 +0200

    modifier les fonction doublement chainee

--- listedbchainee/app/main.go
+++ listedbchainee/app/main.go
@@ -9,37 +9,57 @@ import (
 
 
 func main() {
-	liste := listedbchainee.Liste{}
+
+// test la fonction remove avec cas la liste est vide :
+
+    liste1 := listedbchainee.Liste{}
+	liste1.Remove(2)
+	fmt.Println("liste1 =", liste1)
+
+
+
+// test la fonction cons qui ajoute elm
+ 	liste := listedbchainee.Liste{}
 	fmt.Println(liste)
 	liste.Cons(15)
 	liste.Cons(3)
 	liste.Cons(12)
 	liste.Cons(2)
 	fmt.Println(liste)
+	fmt.Println(liste.ReverseString())
+// test la longueur de la liste 
 	fmt.Printf("Longueur de la liste = %d\n", liste.Length())
 
 
+// test la fonction remove le cas l'elm est millieu de la liste 
+	fmt.Println("test remove avec elm au milieu ")
+	liste.Remove(12)
+	fmt.Println("liste =", liste)
 
-    liste1 := listedbchainee.Liste{}
-	liste1.Remove(2)
-	fmt.Println("liste1 =", liste1)
+//teste cas elt est en tête de la liste 
+	liste.Remove(2)
+	fmt.Println( liste)
+//teste cas elt est en queue de la liste 
+	liste.Remove(15)
+	fmt.Println( " la liste après la supression de dernier de la liste" ,liste)
+	
+
+// test de nouveau la fonction length 
+	fmt.Printf("Longueur de la liste = %d\n", liste.Length())
+
+// test la fonction fromSlice :
+	liste3 := listedbchainee.FromSlice([]int{10, 20, 3, 1})
+	fmt.Println(liste3)
+	fmt.Println(liste3.ReverseString())
+
+
+
+// test fonction append 	
+	/*fmt.Println("test append ")
 	liste.Append(100)
 	liste.Append(200)
 	fmt.Println("liste =" ,liste)
-
-	liste.Remove(2)
-	fmt.Println("liste =", liste)
-	liste.Cons(2)
-	liste.Remove(3)
-	fmt.Println("liste =", liste)
-	liste.Remove(15)
-	fmt.Println("liste =", liste)
-
-	fmt.Printf("Longueur de la liste = %d\n", liste.Length())
-
-
-	liste3 := listedbchainee.FromSlice([]int{10, 20, 3, 1})
-	fmt.Println(liste3)
-
+	fmt.Println(liste.ReverseString())
+	*/
 }
 
--- listedbchainee/listedbchainee.go
+++ listedbchainee/listedbchainee.go
@@ -6,7 +6,7 @@ import (
 
 type cellule struct {
 	valeur  int
-	suivant *cellule
+	suivant *cellule			
 	precedant *cellule
 }
 
@@ -15,7 +15,7 @@ func (c cellule) String() string {
 	return fmt.Sprintf("%d", c.valeur)
 }
 
-// Liste est un type liste simplement chaînée
+// Liste est un type liste doublement  chaînée
 type Liste struct {
 	tete,queue *cellule
 	nbElts int 
@@ -34,14 +34,49 @@ func (ls Liste) String() string {
 	res += "nil}"
 	return res
 }
+
+func (ls Liste) ReverseString() string {
+	res := "{"
+	curr := ls.queue
+	for curr != nil {
+	//1.traitement de la cellule courante :
+		res +=  curr.String()+"->"
+	//2.passer à la cellule precedent
+		curr = curr.precedant		
+	}
+	res += "nil}"
+	return res
+}
 // Cons ajoute un elément en tête de la liste
 func (ls *Liste) Cons(elt int) {
-	nouv := &cellule{elt , ls.tete , ls.queue}
-	ls.tete = nouv	
-	nouv = nil	
-	ls.nbElts ++
-}
 
+	nouv := &cellule{elt , ls.tete , nil}
+	if (ls.Empty()){
+		ls.tete = nouv
+		ls.queue = nouv
+	}else{
+		ls.tete.precedant = nouv
+		ls.tete = nouv
+	}
+		
+	ls.nbElts++
+}
+// Append ajoute elt à la fin de la liste
+func (ls *Liste) Append(elt int) {
+	nouv := &cellule{elt, ls.queue ,ls.tete}
+
+	// Cas particulier : ls était vide
+	if ls.tete == nil {
+		ls.tete = nouv
+		ls.queue = nouv 
+		// Cas général 
+	}else{
+		ls.queue.suivant = nouv
+		nouv.precedant = ls.tete
+		ls.queue = nouv
+	}	
+	ls.nbElts++
+}
 
 // Length renvoie le nombre d'éléments dans la liste
 func (ls Liste) Length() int {
@@ -56,26 +91,7 @@ func (ls Liste) Empty() bool {
 }
 
 
-// Append ajoute elt à la fin de la liste
-func (ls *Liste) Append(elt int) {
-	nouv := &cellule{elt, ls.queue ,ls.tete}
 
-	// Cas particulier : ls était vide
-	if ls.tete == nil {
-		ls.tete = nouv
-		ls.nbElts ++
-		return
-	}
-
-	// Cas général : on se place sur la dernière cellule et on lui ajoute un lien vers nouv
-	curr := ls.tete
-	for curr.suivant != nil {
-		curr = curr.suivant
-	}
-	curr.suivant = nouv
-	ls.nbElts ++
-		
-}
 
 // Remove supprime un élément dans la liste
 func (ls *Liste) Remove(elt int) {
@@ -86,35 +102,38 @@ func (ls *Liste) Remove(elt int) {
 		// Cas particulier 2 : l'elt  est en tete
 	if ls.tete.valeur == elt {
 		ls.tete = ls.tete.suivant
+		ls.tete.precedant = nil
 		ls.nbElts--
 		return
-		}
+	}
+		// Cas particulier 3 : l'elt  est en queue
+	if ls.queue.valeur == elt {
+		ls.queue = ls.queue.precedant
+		ls.queue.suivant = nil
+		ls.nbElts--
+		return
+	}
 	// cas générale 
 	curr := ls.tete
-	prec := curr
+	
 	for curr != nil && curr.valeur != elt {
-		prec = curr
 		curr = curr.suivant 
-		
 	}
 	if (curr != nil && curr.valeur == elt){
-		curr = curr.suivant
-		prec.suivant = curr
-		curr = prec.suivant
+		curr.suivant.precedant = curr.precedant
+		curr.precedant.suivant = curr.suivant
 		ls.nbElts--
 	}
-
 }
 // ToSlice crée une tranche à partir d'une liste
-func (ls Liste) ToSlice() []int {
-	res := []int{}
-	curr := ls.tete
-	for curr != nil {
-		res = append(res, curr.valeur)
-		curr = curr.suivant
+/*func (ls Liste) ToSlice() []int {
+	res:= make([]int, ls.nbElts)
+	i := 0
+	for courant := ls.tete; courant != nil; courant =courant.suivant {
+		res[i] = courant.suivant
 	}
 	return res
-}
+}*/
 
 // FromSlice crée une liste à partir d'une tranche
 func FromSlice(tab []int) Liste {
@@ -123,6 +142,8 @@ func FromSlice(tab []int) Liste {
 		res.Cons(tab[i])
 	}
 	return res
+
+ 
 }
 
 
